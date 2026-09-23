e0f483a136ae9e075430b0e573e9b21670eb7128
marianasoajo <marianasoajo@gmail.com>
2026-09-23 17:33:29 +0100

    fix: update import path for supabase in studentApi.js

--- src/components/students/api/studentApi.js
+++ src/components/students/api/studentApi.js
@@ -1,4 +1,4 @@
-import { supabase } from '../../lib/supabase'
+import { supabase } from '../../../lib/supabase'
 
 export const fetchMetadata = async () => {
     const [yearsRes, formsRes] = await Promise.all([
